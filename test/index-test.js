61492668a11b9ebad4c407c3c06a42b0cc8b8b30
James Stamps Jr <jstampshere@gmail.com>
2020-01-24 22:11:53 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -9,7 +9,7 @@ describe('index', () => {
   const src = path.resolve(__dirname, '..', 'console.error("HALP!")', )
 
   it('calls console.error()', done => {
-    const spy = expect.spyOn(console, 'console.error("Broken!")').andCallThrough()
+    const spy = expect.spyOn(console, '("Broken!")').andCallThrough()
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
